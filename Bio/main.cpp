8ec3652d17eee6f753688af1228cc65f0980bcd9
Trintal <iliyalev@yandex.ru>
2017-12-14 20:48:20 +0300

    Забыл запушить

--- Bio/main.cpp
+++ Bio/main.cpp
@@ -7,6 +7,33 @@ using namespace std;
 
 const char ACGT[4] = { 'A', 'C', 'G', 'T' };
 
+
+
+void Refresh(int*&ind, int len);
+int Hamming(string s1, string s2);
+int totalHam(string *DNA, string v, int t);
+void FindMedian(string &median, string *DNA, int num, int len, int n);
+
+int main() {
+	string size;
+	string *DNA;
+	string median;
+	int len;
+	int num = 0;
+	ifstream fin2("input.txt", ios_base::in);
+	while (fin2 >> size)
+		num++;
+	ifstream fin("input.txt", ios_base::in);
+	fin >> len;	
+	DNA = new string[num]; // [Num("input.txt")];
+	int j = 0;
+	while (getline(fin, DNA[j])) j++;
+	FindMedian(median, DNA, j, len, DNA[0].length());
+	delete[]DNA;
+	cout << "median " << median << endl;
+	return 0;
+}
+
 void Refresh(int*&ind, int len)
 {
 	for (int j = 0; j < len; j++)
@@ -17,39 +44,6 @@ void Refresh(int*&ind, int len)
 		ind[j] = 0;
 	}
 }
-
-int Hamming(string s1, string s2)
-{
-	int res = 0;
-	for (int i = 0; i < s1.length(); i++)
-	{
-		if (s1[i] != s2[i])
-			res++;
-	}
-	return res;
-}
-int totalHam(string *DNA, string v, int t)
-{
-	int res = 0;
-	int Finalres = 0;
-	int min = v.length()+ 1;
-
-	for (int i = 1; i < t; i++)
-	{
-		for (int j = 0; j < DNA[1].length() - v.length() + 1; j++)
-		{
-			if (Hamming(v, DNA[i].substr(j, v.length())) < min)
-			{
-				min = Hamming(v, DNA[i].substr(j, v.length()));
-			}
-		}
-		Finalres += min;
-		min = v.length() + 1;
-
-	}
-	return Finalres;
-}
-
 
 void FindMedian(string &median, string *DNA, int num, int len, int n)
 {
@@ -79,27 +73,41 @@ void FindMedian(string &median, string *DNA, int num, int len, int n)
 			bestWord = pattern;
 		}
 		pattern = "";
-		Refresh(ind, len);	
+		Refresh(ind, len);
 	}
 	median = bestWord;
 }
 
-int main() {
-	string size;
-	string *DNA;
-	string median;
-	int len;
-	int num = 0;
-	ifstream fin2("input.txt", ios_base::in);
-	while (fin2 >> size)
-		num++;
-	ifstream fin("input.txt", ios_base::in);
-	fin >> len;	
-	DNA = new string[num]; // [Num("input.txt")];
-	int j = 0;
-	while (getline(fin, DNA[j])) j++;
-	FindMedian(median, DNA, j, len, DNA[0].length());
-	delete[]DNA;
-	cout << "median " << median << endl;
-	return 0;
+int Hamming(string s1, string s2)
+{
+	int res = 0;
+	for (int i = 0; i < s1.length(); i++)
+	{
+		if (s1[i] != s2[i])
+			res++;
+	}
+	return res;
 }
+int totalHam(string *DNA, string v, int t)
+{
+	int res = 0;
+	int Finalres = 0;
+	int min = v.length() + 1;
+
+	for (int i = 1; i < t; i++)
+	{
+		for (int j = 0; j < DNA[1].length() - v.length() + 1; j++)
+		{
+			if (Hamming(v, DNA[i].substr(j, v.length())) < min)
+			{
+				min = Hamming(v, DNA[i].substr(j, v.length()));
+			}
+		}
+		Finalres += min;
+		min = v.length() + 1;
+
+	}
+	return Finalres;
+}
+
+
